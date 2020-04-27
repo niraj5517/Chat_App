c76363623fc6bd6318efbcff52cb8611474d337d
Niraj Oswal <niraj5517@gmail.com>
2020-04-27 14:18:01 +0530

    trying to commit client

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
+
+import Join from './Join/Join';
+import Chat from './Chat/Chat';
+
+function App() {
+  return (
+    <div>
+      
+     
+ {/* <Router>
+   
+
+<Route  path='/'  component={Join}/>
+<Route  path='/chat'  component={Chat}/>
+   
+    
+   dfgdfh
+ </Router> */}
+
+ </div>
+  );
+}
+
+export default App;
